278d826d957cc19c1b442ad7cd8bd569cf68e9b4
kavinteng <76694895+kavinteng@users.noreply.github.com>
2022-03-31 14:12:49 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ list_url = [None,
             'https://globalapi.advice.co.th/api/upload_people',
             'http://ec2-44-201-34-174.compute-1.amazonaws.com/count_person']
 list_cap = [0,1,'Commart1.mp4','Commart2.mp4','ROB09760.jpg']
-main(url=list_url[0], cap=list_cap[1], display_alltime=True, display_out=False, time_ref=10, line_notify=60)
+main(url=list_url[2], cap=list_cap[0], display_alltime=True, display_out=False, time_ref=10, line_notify=60)
